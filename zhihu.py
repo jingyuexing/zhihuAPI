55900bc0db66110550d537b62901d1b518f70f65
jingyuexing <1941755817@qq.com>
2020-04-22 18:40:40 +0800

    完善目前的API增加用户，文章，回答类

--- zhihu.py
+++ zhihu.py
@@ -2,7 +2,7 @@
 # @Author: jingyuexing
 # @Date:   2020-04-21 18:24:20
 # @Last Modified by:   jingyuexing
-# @Last Modified time: 2020-04-21 20:30:06
+# @Last Modified time: 2020-04-22 18:21:27
 
 import json
 import urllib3
@@ -21,8 +21,8 @@ with open('data/API.json','r',encoding='utf-8') as file:
     api = json.loads(file.read())
     file.close()
 
-def requests(method='',url='',parma={}):
-    req = http.request(method=method,url=url,parma=parma)
+def requests(method='',url='',param={}):
+    req = http.request(method=method,url=url,fields=param)
     if(req.status==200):
         return json.loads(req.data.decode('utf-8'),encoding='utf-8')
 
@@ -30,45 +30,77 @@ def GetHotList(limit=30):
     config = api[0]
     method = config['method']
     url = config['link']
-    parma = {
+    param = {
         'limit':limit,
         "desktop":"true"
     }
-    return requests(method=method,url=url,parma=parma)
+    return requests(method=method,url=url,param=param)
 
 
 def getUserActive(user='',limit=10):
     config = api[1]
     url = config['link'].format(user=user)
-    parma = {
+    method = config['method']
+    param = {
         "limit":limit,
         'desktop':'true'
     }
-    return requests(method=method,url=url,parma=parma)
+    return requests(method=method,url=url,param=param)
 
 def getAnswerComment(answerID=0,limit=20,offset=0,status='open'):
     config = api[3]
     url = config['link'].format(answersID=answerID)
     method = config['method']
-    parma = {
+    param = {
         "limit":limit,
         "offset":offset,
         "status":status
     }
-    return requests(method=method,url=url,parma=parma)
+    return requests(method=method,url=url,param=param)
+
+def getUserInfo(username=''):
+    if(username!=''):
+        config = api[4]
+        method = config['method']
+        url = config['link'].format(username=username)
+        param = {
+            "include":"allow_message%2Cis_followed%2Cis_following%2Cfollowing_count%2Cis_org%2Cis_blocking%2Cemployments%2Canswer_count%2Cfollower_count%2Carticles_count%2Cgender"
+        }
+        return requests(method=method,url=url,param=param)
+def getUserFollowers(username='',offset=0,limit=20):
+    if(username!=''):
+        config = api[5]
+        method = config['method']
+        url = config['link'].format(username=username)
+        param = {
+            "limit":limit,
+            'offset':offset,
+            "include":"data[*].answer_count,articles_count,gender,is_followed,following_count,is_following,badge[?(type=best_answerer)].topics"
+        }
+        return requests(method=method,url=url,param=param)
+
 
 
 class User:
-    userId = ''
-    name = ''
-    fansNumber = 0
-    answerNumber = 0
-    articles = 0
-    vip = False
+    userId = '' #用户id
+    name = '' #名称
+    fansNumber = 0 #粉丝数
+    answerNumber = 0 #回答数
+    following = 0  #关注的人
+    articles = 0 #文章数
+    face = '' #头像
+    vip = False #是否是VIP
     """docstring for User"""
     def __init__(self,userName=''):
-        pass
-
+        data = getUserInfo(username=userName)
+        if(data!=None):
+            self.userId = data['id']
+            self.articles = data['articles_count']
+            self.answerNumber = data['answer_count']
+            self.face = data['avatar_url_template']
+            self.vip = data['vip_info']['is_vip']
+            self.following = data['following_count']
+            self.fansNumber = data['follower_count']
 
 class Answer:
     """docstring for Answer"""
@@ -82,4 +114,4 @@ class Articles:
     def __init__(self, arg):
         pass
         
-                        
+
